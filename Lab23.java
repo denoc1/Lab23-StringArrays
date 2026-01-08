d9042c67548b90d18e4ff58f441cf1c055deaffb
denoc1 <176614645+denoc1@users.noreply.github.com>
2026-01-08 03:43:06 +0000

    template

---
+++ Lab23.java
@@ -0,0 +1,11 @@
+public class Lab23{
+
+//you have two required methods and four optional methods. 
+//only include the method headers for the methods you complete.
+
+
+    public static void main(String[] args){
+
+
+    }
+}
